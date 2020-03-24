073565f099523108fc012b0f2cccd82885a2dcde
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-03-24 14:38:28 +0100

    creazione html e commenti js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova
+
+// creo array cognomi
+
+// chiedo all'utente il suo cognome
+
+// inserisco il cognome dell'utente nella lista array
+
+// stampo una lista ordinata alfabeticamente
+
+// far ritornare l'indice dell'elemento all'interno dell'array ordinato
